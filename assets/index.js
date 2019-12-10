b1f9829685a96481bdd20fab1f6c3c4fc85b5a66
Fernando Moreno <jeison.jfmt@gmail.com>
2019-12-10 02:24:12 -0500

    DOM

--- assets/index.js
+++ assets/index.js
@@ -13,28 +13,55 @@ form.addEventListener ("submit", function (e) {
         name.style.background ="darkred";
         name.placeholder = "El nombre es muy corto";         
     }
+
+    name.addEventListener("click", function () {
+        this.style.background ="";
+        this.placeholder = "Nombre"; 
+    })
+    
     if (apellido.value === "" || apellido.value === null || apellido.value.length < 3) {
         e.preventDefault();
         apellido.style.background ="darkred";
         apellido.placeholder = "El apellido es muy corto";    
     }
+
+    apellido.addEventListener("click", function () {
+        this.style.background ="";
+        this.placeholder = "Apellido"; 
+    })
+
     if (email.value === "" || email.value === null ) {
         e.preventDefault();
         email.style.background ="darkred";
         email.placeholder = "Ingrese un e-mail válido";     
     }
+    
+    email.addEventListener("click", function () {
+        this.style.background ="";
+        this.placeholder = "e-mail"; 
+    })
 
     if (pass.value === "" || pass.value === null || pass.value.length < 8) {
         e.preventDefault();
         pass.style.background ="darkred";
         pass.placeholder = "Contraseña muy corta";
     }
+    pass.addEventListener("click", function () {
+        this.style.background ="";
+        this.placeholder = "Contraseña (8 carácteres)"; 
+    })
+
     if (tel.value === "" || tel.value === null || tel.value.length < 7) {
         e.preventDefault();
         tel.style.background ="darkred";
         tel.placeholder = "El teléfono no es válido"; 
     }
+    tel.addEventListener("click", function () {
+        this.style.background ="";
+        this.placeholder = "Teléfono"; 
+    })
     window.localStorage.setItem("Nombre", name.value);
+    
 })
 
 
--- welcome/assets/welcome.js
+++ welcome/assets/welcome.js
@@ -4,16 +4,44 @@ nombre = window.localStorage.getItem("Nombre");
 
 wel.innerHTML = nombre;
 
-const form = document.querySelector("#form"),
-    task = document.querySelector("#tarea"),
-    añadir = document.querySelector("#añadir"),
-    agregar= document.querySelector("#agregar");
 
-form.addEventListener("submit", (e)=>{
-    if (task.value.length > 0) {
-        agregar.innerHTML = task.value;
-    }else{
-        task.style.background = "red";
-    }
-})
+const task = document.querySelector("#tarea"),
+    btnAñadir = document.querySelector("#btn_añadir"),
+    lista= document.querySelector("#agregar");
 
+    let agregarTarea = function () {
+        let tarea = task.value,
+        nuevaT = document.createElement("li"),
+        ancla = document.createElement("a");
+        content = document.createTextNode(tarea);
+
+        if (tarea === "" || tarea === null) {
+            task.placeholder = "La tarea es muy corta";
+            task.style.background = "darkred";
+            return false;
+        };
+
+        ancla.appendChild(content);
+        ancla.setAttribute("href", "#");
+        nuevaT.appendChild(ancla);
+        lista.appendChild(nuevaT);
+
+        task.value ="";
+
+        for (let i = 0; i <= lista.children.length -1; i++) {
+            lista.children[i].addEventListener("click", function () {
+                this.parentNode.removeChild(this);
+            });
+            
+        };
+    };
+
+    let validar = function () {
+        this.style.background = "";
+        this.placeholder = "Agrega tu tarea";
+    };
+ 
+
+btnAñadir.addEventListener("click", agregarTarea);
+
+task.addEventListener ("click",validar);
